62bc79eabe449492afd8f1ac919cae23e7ef1812
neville.walo@gmail.com <neville.walo@gmail.com>
2018-09-22 23:22:14 +0200

    Test1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World1!");
         System.out.print("Hello World");
+        System.out.print("Test2");
     }
 }
