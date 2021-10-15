12f9af08a4b976c442ca59369f6f75659a17a9f5
Neville Walo <neville.walo@gmail.com>
2021-10-15 14:32:28 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World1!");
-        System.out.print("Hello World6");
+        System.out.print("Hello World7");
         System.out.print("Test2");
     }
 }
